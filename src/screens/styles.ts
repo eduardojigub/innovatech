144af0ee3543e791323e0ce6c739368f60151c8b
Eduardo Sousa <eduardobrunoit@gmail.com>
2024-05-20 23:54:01 -0400

    feat: fix safeAreaView

--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -1,7 +1,7 @@
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #f0f0f0;
   padding: 10px;
