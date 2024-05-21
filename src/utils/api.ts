e03065b50c71fcac8f2af94077f1f91a5ba9353b
Eduardo Sousa <eduardobrunoit@gmail.com>
2024-05-21 02:08:58 -0400

    fix: add template string na api call per page

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,8 +3,7 @@ import { User } from "@interfaces/user";
   
   export const fetchUsers = async (page: number): Promise<User[]> => {
     try {
-      {console.log('chamou o fetchUsers')}
-      const response = await fetch('https://randomuser.me/api/?page=${page}&results=20&seed=eduardobrunov1');
+      const response = await fetch(`https://randomuser.me/api/?page=${page}&results=20&seed=eduardobrunov1`);
       if (!response.ok) //boolean que indica se a resposta foi bem sucedida (status 200-299) ou não. 
         {
         throw new Error('Erro ao buscar usuários');
