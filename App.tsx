e7ea8b0c026aa6ab5200f2c41a0c1f986d20aca0
Eduardo Sousa <eduardobrunoit@gmail.com>
2024-05-23 02:40:42 -0400

    feat: add expo splash screen to fix splash screen render

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import MainScreen from '@screens/MainScreen';
 import { UserProvider } from '@context/userContext';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
+
 
 export default function App() {
   return (
